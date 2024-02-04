0d8949945c56a8d622f136d20b9de8d94b06c013
Carlos Garcia <carlosviniciogarcia1997@gmail.com>
2024-02-04 11:52:20 -0500

    Moved from ./db/createNewWallet.js to this one

--- commands/hdwalletStart.js
+++ commands/hdwalletStart.js
@@ -3,15 +3,16 @@ const colors = require('colors')
 const bip39 = require("bip39")
 const ecc = require('tiny-secp256k1');
 const crypto = require('crypto');
+const utxolib = require('@bitgo/utxo-lib')
+const ecashaddrjs = require("ecashaddrjs");
 
 const {BIP32Factory} = require("bip32");
-const {log} = require('../configs/constants');
-const createNewWallet = require('../db/createNewWallet');
+const {log, derivationPath} = require('../configs/constants');
+const getConnection = require('../db/getConnection');
 
 class Set {
     generate = async ()=>{
-        /* const initialDerivationPath = "m/44'/0'/0'/0";
-        const bip32 = BIP32Factory(ecc); */
+        const {fromString} = await import('uint8arrays');
         
         log("This is your mnemonic, save in a safe place: ")
         const mnemonic = bip39.generateMnemonic(128)
@@ -64,7 +65,53 @@ class Set {
         encryptedMnemonic += cipher.final('hex');
 
         log("Your encrypted mnemonic is " + encryptedMnemonic)
-        await createNewWallet(encryptedMnemonic,iv, newUser.password, )
+
+        const dbAccount = await getConnection("account")
+
+        if(dbAccount.data.account.length > 0) {
+            log("Do you want to change your current mnemonic for another?")
+            return
+        }
+        console.log("Create new wallet ")
+
+        const bip32 = BIP32Factory(ecc);
+        const seedBuffer = bip39.mnemonicToSeedSync(mnemonic)
+
+        const hdNode = utxolib.bip32.fromSeed(seedBuffer);
+        const derivedNode = hdNode.derivePath("m/44'/0'/0'/0");
+
+        //const masterKey = bip32.fromSeed(seedBuffer)
+        const xPub = derivedNode.neutered().toBase58();
+
+        await dbAccount.update(({ account }) => account.push({mnemonic: encryptedMnemonic, iv, xPub}))
+
+        //Almacenar la primera dirección
+
+        // Utilizar la misma clave y IV que se usaron para cifrar
+        const cipherKey2 = crypto.createHash('sha256').update(newUser.password).digest();
+        // Crear el descifrador utilizando createDecipheriv
+        const decipher = crypto.createDecipheriv('aes-256-cbc', cipherKey2, iv);
+        // Descifrar el texto
+        let decryptedMnemonic = decipher.update(encryptedMnemonic, 'hex', 'utf8');
+        decryptedMnemonic += decipher.final('utf8');
+
+        console.log("Your decrypted mnemonic is " + decryptedMnemonic);
+
+        const masterKey = bip32.fromSeed(seedBuffer)
+        const initialDerivationPath = derivationPath
+        const publicKey = masterKey.derivePath(initialDerivationPath)
+        
+        const legacyAddress = utxolib.payments.p2pkh({ pubkey: publicKey.derive(0).publicKey }).address;
+        const hash = utxolib.address.fromBase58Check(legacyAddress).hash.toString('hex')
+        //log("uint8arrays: ",uint8arrays)
+        const uint8array = fromString(hash, 'hex')
+        const ecashAddress = ecashaddrjs.encode("ecash", "P2PKH" , uint8array)
+        console.log('Dirección 0:', ecashAddress);
+
+        const dbAddresses = await getConnection("addresses")
+        
+        await dbAddresses.update(({ addresses }) => addresses.push(ecashAddress))
+        console.log("Db created successfully")
     }
 
 }
