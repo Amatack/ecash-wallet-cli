dd07c9f62aefd6c96a5cf9d0f991f362ee7d6d2c
Carlos Garcia <carlosviniciogarcia1997@gmail.com>
2024-02-04 22:57:49 -0500

    chronikInstance url added

--- configs/constants.js
+++ configs/constants.js
@@ -1,7 +1,9 @@
 const log = console.log
 const derivationPath = "m/44'/0'/0'/0";
+const chronikInstance = "https://chronik.be.cash/xec"
 
 module.exports = {
+    chronikInstance,
     derivationPath,
     log,
 }
